9f1ac79a2ab67b2367e9f3e0a045d8a64f1de790
54215 <54215@HCHENG>
2019-09-02 09:38:37 +0800

    添加项目文件。

---
+++ ServiceInstaller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceInstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
