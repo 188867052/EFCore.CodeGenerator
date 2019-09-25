ca239cef3fd41714db818d39c041849aa42fb40d
188867052 <188867052@qq.com>
2019-09-25 03:30:14 +0000

    generated by generator pipeline

--- EFCore.Scaffolding.Extension.Entity/Log.cs
+++ EFCore.Scaffolding.Extension.Entity/Log.cs
@@ -5,10 +5,19 @@ namespace Entities
 {
     public partial class Log
     {
+        /// <summary>
+        /// 主键.
+        /// </summary>
         public Guid Identifier { get; set; }
 
+        /// <summary>
+        /// 日志类容.
+        /// </summary>
         public string Message { get; set; }
 
+        /// <summary>
+        /// 创建时间.
+        /// </summary>
         public DateTime? CreateTime { get; set; }
     }
 }
