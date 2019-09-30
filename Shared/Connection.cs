0b14e61df69042be8ea87b7d66e018716c854000
召 程 <542153354@qq.com>
2019-09-30 10:44:30 +0800

    update

--- Shared/Connection.cs
+++ Shared/Connection.cs
@@ -6,31 +6,40 @@ namespace EFCore.Scaffolding.Extension
 {
     public static class Connection
     {
-        private const bool IsDevelop = false;
         private static string aliCloud;
-        private const string Local = @"Data Source=HCHENG\SQLEXPRESS;Initial Catalog=Scaffolding;Integrated Security=True";
+        private const string LocalConnectionString = @"Data Source=HCHENG\SQLEXPRESS;Initial Catalog=Scaffolding;Integrated Security=True";
 
         public static string ConnectionString
         {
             get
             {
-                return IsDevelop ? Local : AliCloud;
-            }
-        }
-
-        private static string AliCloud
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(aliCloud))
+                if (!string.IsNullOrEmpty(aliCloud))
                 {
-                    var di = new DirectoryInfo(Environment.CurrentDirectory);
-                    string file = Directory.GetFiles(di.Parent.Parent.Parent.Parent.FullName, "ConnectionString.txt", SearchOption.AllDirectories).FirstOrDefault();
-                    aliCloud = File.ReadAllText(file);
+                    return aliCloud;
+                }
+
+                string filePath = GetPipelineFile();
+                bool isAzure = string.IsNullOrEmpty(filePath);
+                if (isAzure)
+                {
+                    if (string.IsNullOrEmpty(aliCloud))
+                    {
+                        aliCloud = File.ReadAllText(filePath);
+                    }
+                }
+                else
+                {
+                    return LocalConnectionString;
                 }
 
                 return aliCloud;
             }
+        }
+
+        private static string GetPipelineFile()
+        {
+            var di = new DirectoryInfo(Environment.CurrentDirectory);
+            return Directory.GetFiles(di.Parent.Parent.Parent.Parent.FullName, "ConnectionString.txt", SearchOption.AllDirectories).FirstOrDefault();
         }
     }
 }
