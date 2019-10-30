4e2f311947a17624007d616a38578ca3cc064048
召 程 <542153354@qq.com>
2019-10-30 14:20:00 +0800

    add database file.

--- Shared/Connection.cs
+++ Shared/Connection.cs
@@ -7,7 +7,7 @@ namespace EFCore.Scaffolding.Extension
     public static class Connection
     {
         private static string aliCloud;
-        private const string LocalConnectionString = @"Data Source=HCHENG\SQLEXPRESS;Initial Catalog=Scaffolding;Integrated Security=True";
+        private const string LocalConnectionString = @"Data Source=.;Initial Catalog=Scaffolding;Integrated Security=True";
 
         public static string ConnectionString
         {
